58a01f91fc696a96faab16ef7916153de3733276
Piyush Kumar Singh <64677706+Piyush98123@users.noreply.github.com>
2026-04-04 17:25:30 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+          if(s.length()!=t.length()) return false;
+        char A[] = s.toCharArray();
+        char B[] = t.toCharArray();
+        int freq[]=new int[26];
+        for(char ch : A){
+            freq[ch-'a']++;
+        }
+        for(char ch : B){
+            if(freq[ch-'a']==0) return false;
+            freq[ch-'a']--;
+        }
+        return true;
+    }
+}
